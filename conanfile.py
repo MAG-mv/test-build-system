2b5956d5ad2ab734c3fde8ff83fd1b44ef4567fd
MAG-mv <matthew.voisey@magnopus.com>
2026-03-06 14:26:27 +0000

    updating to use conan py file to pass through build shared libs

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class TestLib(ConanFile):
+    name = "test-lib"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+
+    exports_sources = "CMakeLists.txt", "Library/*", "Tests/*", "cmake/*"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        pass
+
+    def build_requirements(self):
+        self.test_requires("gtest/1.17.0")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
